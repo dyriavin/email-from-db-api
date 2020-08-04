9ddb2acbad46b6358ada812b41d7bdf8f5036bd1
Dyriavin <dyriavin@gmail.com>
2020-08-05 02:39:06 +0300

    Credit model implementation

--- app/Http/Controllers/UserCreditController.php
+++ app/Http/Controllers/UserCreditController.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\UserCredit;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserCreditController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,14 +14,19 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('user.home');
+        //
     }
 
-    private function checkCredit()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $user = User::find(auth()->id());
-
+        //
     }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -37,10 +41,21 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\UserCredit  $userCredit
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(UserCredit $userCredit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserCredit  $userCredit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserCredit $userCredit)
     {
         //
     }
@@ -49,10 +64,10 @@ class UserController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\UserCredit  $userCredit
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, UserCredit $userCredit)
     {
         //
     }
@@ -60,10 +75,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\UserCredit  $userCredit
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(UserCredit $userCredit)
     {
         //
     }
---
+++ database/migrations/2020_08_04_233359_create_user_credits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCreditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_credits', function (Blueprint $table) {
+            $table->id();
+            $table->integer('credit')->default(20000);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_credits');
+    }
+}
