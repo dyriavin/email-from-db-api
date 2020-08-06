1853c70981ca86a06b3d5aaa96985eb9e42a3847
Dyriavin <dyriavin@gmail.com>
2020-08-06 21:13:11 +0300

    UI fix for layout

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <h1> User Side </h1>
+    <h1> Список email  </h1>
     <table class="table table-hover">
         <thead>
         <tr>
             <th scope="col">#</th>
             <th scope="col">Email</th>
-            <th scope="col">Sender Email</th>
-            <th scope="col">Delivery Status</th>
-            <th scope="col">Send Date</th>
+            <th scope="col">Email отправителя</th>
+            <th scope="col">Статус доставки </th>
+            <th scope="col">Дата отправки</th>
         </tr>
         </thead>
         <tbody>
