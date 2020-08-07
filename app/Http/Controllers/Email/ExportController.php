5039a5b48c63e964db87bf12587dc105b6909a39
Dyriavin <dyriavin@gmail.com>
2020-08-07 03:37:43 +0300

    Implemented scheduled funding the account for the user

--- app/Http/Controllers/Email/ExportController.php
+++ app/Http/Controllers/Email/ExportController.php
@@ -3,38 +3,40 @@
 namespace App\Http\Controllers\Email;
 
 use App\Http\Controllers\Email\EmailController;
+use App\Http\Controllers\UserCreditController;
+use App\Jobs\UpdateCreditBalance;
 use App\Models\Email;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ExportController extends BaseEmailController
 {
-    public function export(? string $from = null,? string $to = null)
+    public function export(?string $from = null, ?string $to = null)
     {
 
-        $fileName = Carbon::now()->toDateString()."-file.csv";
+        $fileName = Carbon::now()->toDateString() . "-file.csv";
         $limit = auth()->user()->credit->credit;
-        $emails = EmailController::getEmails($from,$to,$limit);
+        $emails = EmailController::getEmails($from, $to, $limit);
 
         $headers = [
-            "Content-type"        => "text/csv",
+            "Content-type" => "text/csv",
             "Content-Disposition" => "attachment; filename=$fileName",
-            "Pragma"              => "no-cache",
-            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
-            "Expires"             => "0"
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
         ];
 
         $columns = ['ID', 'EMAIL', 'SENDER EMAIL', 'DELIVERY STATUS ', 'SEND DATE'];
-        $callback = function() use($emails, $columns) {
+        $callback = function () use ($emails, $columns) {
             $file = fopen('php://output', 'w');
             fputcsv($file, $columns);
 
             foreach ($emails['total'] as $email) {
-                $row['ID']  = $email->id;
-                $row['EMAIL']    = $email->email;
-                $row['SENDER EMAIL']    = $email->sender_email;
-                $row['DELIVERY STATUS']  = $email->delivery_status;
-                $row['SEND DATE']  = $email->send_date;
+                $row['ID'] = $email->id;
+                $row['EMAIL'] = $email->email;
+                $row['SENDER EMAIL'] = $email->sender_email;
+                $row['DELIVERY STATUS'] = $email->delivery_status;
+                $row['SEND DATE'] = $email->send_date;
 
                 fputcsv($file, [$row['ID'], $row['EMAIL'], $row['SENDER EMAIL'], $row['DELIVERY STATUS'], $row['SEND DATE']]);
             }
@@ -43,7 +45,9 @@ class ExportController extends BaseEmailController
         };
         $credit = auth()->user()->credit->credit - $limit;
 
-        auth()->user()->credit()->update(['credit'=> $credit]);
+        auth()->user()->credit()->update(['credit' => $credit]);
+
+        UserCreditController::updateCreditBalance(auth()->id());
 
         return response()->stream($callback, 200, $headers);
     }
--- app/Http/Controllers/UserCreditController.php
+++ app/Http/Controllers/UserCreditController.php
@@ -2,84 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\UpdateCreditBalance;
 use App\Models\UserCredit;
 use Illuminate\Http\Request;
 
 class UserCreditController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public static function updateCreditBalance(int $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\UserCredit  $userCredit
-     * @return \Illuminate\Http\Response
-     */
-    public function show(UserCredit $userCredit)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\UserCredit  $userCredit
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(UserCredit $userCredit)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\UserCredit  $userCredit
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, UserCredit $userCredit)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\UserCredit  $userCredit
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(UserCredit $userCredit)
-    {
-        //
+        dispatch(new UpdateCreditBalance($id))->delay(now()->addSeconds(15));
     }
 }
