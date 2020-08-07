9c33423440c8398acf19434429c85ea037a0a014
Dyriavin <dyriavin@gmail.com>
2020-08-08 01:56:04 +0300

    Dashboard final

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,8 +1,40 @@
 @extends('layouts.admin')
 
 @section('content')
+    <h4 class="font-weight-bolder text-uppercase mt-2 text-center">Состояние Базы</h4>
+    <div class="row mb-2">
+        <div class="col-md-4">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-success">Всего</strong>
+                    <h3 class="mb-2 text-success">{{$emailData['total']}}</h3>
+                    <div class="mb-1 text-muted">{{\Carbon\Carbon::now()}}</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary">Использовано</strong>
+                    <h3 class="mb-2 text-primary">{{$emailData['left']}}</h3>
+                    <div class="mb-1 text-muted">{{\Carbon\Carbon::now()}}</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-danger">Использовано</strong>
+                    <h3 class="mb-2 text-danger">{{$emailData['used']}}</h3>
+                    <div class="mb-1 text-muted">{{\Carbon\Carbon::now()}}</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     <h4 class="font-weight-bolder text-uppercase mt-5 text-center">Автоматические пополнения баланса</h4>
-{{--    {{dd($jobs)}}--}}
+
+
     <table class="table table-striped table-dark">
         <thead>
         <tr>
