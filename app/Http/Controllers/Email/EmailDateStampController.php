4eaac21dffb0f8a00e2463538be7b51ddf9f5753
Dyriavin <dyriavin@gmail.com>
2020-08-10 00:41:10 +0300

    Added: - MCR for email_date_stamps - Will be used before export

---
+++ app/Http/Controllers/Email/EmailDateStampController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Email;
+
+use App\Models\EmailDateStamp;
+use Illuminate\Http\Request;
+
+class EmailDateStampController extends BaseEmailController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EmailDateStamp  $emailDateStamp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EmailDateStamp $emailDateStamp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EmailDateStamp  $emailDateStamp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EmailDateStamp $emailDateStamp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EmailDateStamp  $emailDateStamp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EmailDateStamp $emailDateStamp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EmailDateStamp  $emailDateStamp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EmailDateStamp $emailDateStamp)
+    {
+        //
+    }
+}
