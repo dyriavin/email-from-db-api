844b560b4135218b1dc06800f93ae1e23150e73e
Dyriavin <dyriavin@gmail.com>
2020-08-12 18:46:54 +0300

    Update form for search.blade.php

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -6,6 +6,7 @@
 
     <form method="post" action="{{route('email.search')}}">
         @csrf
+
         <div class="form-group">
             <label>Начальная дата:</label>
             <input type="date" name="start_date" max="2020-06-30"
@@ -17,6 +18,21 @@
             </label>
             <input class="form-control" id="key" name="key"
                    type="text" placeholder="ключ">
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="user_id">User ID</label>
+                <input type="number" class="form-control" id="user_id" name="user_id">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="mailing_id">Mailing List ID</label>
+                <input type="number" class="form-control" id="mailing_id" name="mailing_id">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="client_ip">Client IP</label>
+                <input type="text" class="form-control" id="client_ip" name="client_ip">
+            </div>
+
         </div>
         <button type="submit" id="search" class="btn btn-primary">Получить список</button>
     </form>
