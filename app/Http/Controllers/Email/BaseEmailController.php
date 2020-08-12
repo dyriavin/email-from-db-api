2103705883a4785289c5aa138529960d95fd06a6
Dyriavin <dyriavin@gmail.com>
2020-08-12 21:52:28 +0300

    Create new Logic for setting email data params Create global update method

--- app/Http/Controllers/Email/BaseEmailController.php
+++ app/Http/Controllers/Email/BaseEmailController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Email;
 
 use App\Http\Controllers\Controller;
+use App\Models\Email;
 use Illuminate\Http\Request;
 
 class   BaseEmailController extends Controller
 {
-    //
+    public static function insertEmailData(array $emailData)
+    {
+        $records = Email::where('given_to_user','=',0)->take(auth()->user()->credit->credit)->pluck('id');
+
+//        Email::whereIn()->update(['sender_email' => $emailData]);
+
+    }
+
 }
