1928eff7e89b85fb2a7e616c05dff7a386e4fa53
Dyriavin <dyriavin@gmail.com>
2020-08-18 21:21:43 +0300

    new layout of search form

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -12,28 +12,30 @@
             <input type="date" name="start_date" max="2020-06-30"
                    min="2020-01-01" class="form-control">
         </div>
-        <div class="form-group">
-            <label> Ключ:
-                <span class="required" style="color:red" >*</span>
-            </label>
-            <input class="form-control" id="key" name="key"
-                   type="text" placeholder="ключ">
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-4">
-                <label for="user_id">User ID</label>
-                <input type="number" class="form-control" id="user_id" name="user_id">
+
+            <div class="form-group">
+                <label for="key">Ключ</label>
+                <input type="text" name="key" class="form-control" id="key" value="" disabled>
             </div>
-            <div class="form-group col-md-4">
-                <label for="mailing_id">Mailing List ID</label>
-                <input type="number" class="form-control" id="mailing_id" name="mailing_id">
+            <div class="form-group">
+                <label for="sender_email">Email</label>
+                <input type="email" class="form-control" name="sender_email" id="sender_email" >
             </div>
-            <div class="form-group col-md-4">
-                <label for="client_ip">Client IP</label>
-                <input type="text" class="form-control" id="client_ip" name="client_ip">
+            <div class="form-row mb-2">
+                <div class="col-7">
+                    <label for="user_id">User ID</label>
+                    <input type="number" class="form-control" name="user_id" id="user_id" >
+                </div>
+                <div class="col">
+                    <label for="mailing_id">Mailing ID</label>
+                    <input type="number" class="form-control" name="mailing_id" id="mailing_id" >
+                </div>
+                <div class="col">
+                    <label for="client_ip">Client IP</label>
+                    <input type="text" class="form-control" name="client_ip" id="client_ip" >
+                </div>
             </div>
 
-        </div>
         <button type="submit" id="search" class="btn btn-primary">Получить список</button>
     </form>
 
